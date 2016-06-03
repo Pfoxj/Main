d047a6b6754039ca900f99e5c605a8c8a2db40e7
Richard Caceres <richard@archive.org>
2016-06-02 23:36:27 -0700

    Fix a couple issues

--- contents/static/js/scripts.js
+++ contents/static/js/scripts.js
@@ -57,8 +57,8 @@ $(document).ready(function() {
     modalEl.onclick = function(e) {
       if (e.target == modalEl) {
         closeModal();
+        e.preventDefault();
       }
-      e.preventDefault();
     };
     window.onkeydown = function(event) {
       if (event.keyCode === 27 /* esc */) {
